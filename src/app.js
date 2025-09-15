c31b6a7d112181866429ecdfa01294ab5defe969
Saurav Jain <saurav.jain@calsoftinc.com>
2025-09-15 23:17:41 +0530

    2nd commit

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 // Simple route for testing
 app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
+  res.send("Backend is running smoothly");
 });
 
 export default app;
